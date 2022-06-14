07de6cafcb225348ac1ecc685179db865e39eee2
ElifCatakli <92272306+ElifCatakli@users.noreply.github.com>
2022-06-14 18:14:42 +0200

    Add files via upload

---
+++ cameratest.py
@@ -0,0 +1,12 @@
+from picamera import PiCamera
+from time import sleep
+
+camera = PiCamera()
+camera.rotation = 180
+
+camera.start_preview()
+for i in range(5):
+    sleep(5)
+    camera.capture('/home/pi/Desktop/image%s.jpg' % i)
+camera.stop_preview()
+cv2.destroyAllWindows()
