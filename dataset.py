881ff87e2b9d99ea76091384e774df82b2bff937
JathushanRagavan <92302437+JathushanRagavan@users.noreply.github.com>
2022-12-17 17:38:57 +0100

    new Comments + Methodes
    Added new Comments to dataset and facetraining and tried to implement a Method for dataset to call it in facetraining

--- dataset.py
+++ dataset.py
@@ -1,39 +1,65 @@
 import cv2
 import os
 
-camera = cv2.VideoCapture(0)
-camera.set(3, 640) # set video width
-camera.set(4, 480) # set video height
-face_detector = cv2.CascadeClassifier('./haarcascade_frontalface_default.xml')
 
-# For each person, enter one numeric face id
-face_id = input('\n enter user id end press <return> ==>  ')
-print("\n [INFO] Initializing face capture. Look the camera and wait ...")
+def capture_faces(face_id):
+    # Initialize the video capture
+    camera = cv2.VideoCapture(0)
 
-# Initialize face count, filename 
-count = 0
-while(True):
-    ret, frame = camera.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = face_detector.detectMultiScale(gray, 1.3, 5)
-    for (x,y,w,h) in faces:
-        # Cuts part of image to save -> rectangle  
-        cv2.rectangle(frame, (x,y), (x+w,y+h), (255,0,0), 2)     
-        count += 1
-        # Save the captured image into the datasets folder
-        cv2.imwrite("dataset/User." + str(face_id) + '.' +  
-                    str(count) + ".jpg", gray[y:y+h,x:x+w])
-        cv2.imshow('frame', frame)
-    # Waits for user input, prevents a crash  
-    k = cv2.waitKey(100) & 0xff # Press 'ESC' for exiting video
-    if k == 27:
-        break
-    elif count >= 30: # Take 30 face sample and stop video
-         break
-     
-# Do a bit of cleanup
-print("\n [INFO] Exiting Program and cleanup stuff")
-# Closes video file or the capturing device 
-camera.release()
-cv2.destroyAllWindows()
+    # Set the width and height of the video
+    camera.set(3, 640)
+    camera.set(4, 480)
+
+    # Load the haarcascade for frontal face
+    face_detector = cv2.CascadeClassifier('./haarcascade_frontalface_default.xml')
+
+    # Ask the user to enter a face ID
+    #face_id = input('\n enter user id end press <return> ==>  ')
+
+    # Print a message to let the user know that the program is initializing
+    #print("\n [INFO] Initializing face capture. Look the camera and wait ...")
+
+    # Initialize variable for face count and filename 
+    count = 0
+
+    # Capture frames from the camera indefinitely
+    while(True):
+        # Read the current frame from the video capture
+        ret, frame = camera.read()
+        
+        # Convert the frame to grayscale
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        
+        # Detect faces in the frame
+        faces = face_detector.detectMultiScale(gray, 1.3, 5)
+        
+        # Iterate over the detected faces
+        for (x,y,w,h) in faces:
+            # Draw a rectangle around the face 
+            cv2.rectangle(frame, (x,y), (x+w,y+h), (255,0,0), 2)
+            
+            # Increment the face count 
+            count += 1
+            
+            # Save the captured image to the datasets folder
+            cv2.imwrite("dataset/User." + str(face_id) + '.' +  str(count) + ".jpg", gray[y:y+h,x:x+w])
+            
+            # Display the frame with the rectangle around the face
+            cv2.imshow('frame', frame)
+            
+        # Check if the user pressed the 'ESC' key to exit the program, prevents a crash  
+        k = cv2.waitKey(100) & 0xff # Press 'ESC' for exiting video
+        if k == 27:
+            break
+        
+        # Stop capturing faces after 30 frames
+        elif count >= 30: # Take 30 face sample and stop video
+             break
+         
+    # Print a message to let the user know that the program is exiting
+    print("\n [INFO] Exiting Program and cleanup stuff")
+
+    # Release the video capture and close all windows 
+    camera.release()
+    cv2.destroyAllWindows()
 
--- facetraining.py
+++ facetraining.py
@@ -2,36 +2,66 @@ import cv2
 import numpy as np
 from PIL import Image
 import os
+from dataset import capture_faces
+
+#Test Methodes
+# Ask the user to enter a face ID
+face_id = input('\n enter user id end press <return> ==>  ')
+
+# Print a message to let the user know that the program is initializing
+print("\n [INFO] Initializing face capture. Look the camera and wait ...")
+
+capture_faces(face_id)
 
 # Path for face image database
 path = 'dataset'
+
 # LBPH is a method used for face recognition  
 recognizer = cv2.face.LBPHFaceRecognizer_create()
+
+# Load the haarcascade for frontal face detection
 detector = cv2.CascadeClassifier("haarcascade_frontalface_default.xml");
 
-# function to get the images and label data
+# Function to get the images and label data
 def getImagesAndLabels(path):
     # looks for image path
     imagePaths = [os.path.join(path,f) for f in os.listdir(path)]     
     faceSamples=[]
     ids = []
+    
+    # Iterate over the image paths
     for imagePath in imagePaths:
-        PIL_img = Image.open(imagePath).convert('L') # grayscale
+        
+        # Open the images in grayscale
+        PIL_img = Image.open(imagePath).convert('L')
+        
+        # Convert the image to a Numpy array
         img_numpy = np.array(PIL_img,'uint8')
+        
+        # Get the ID from the file name
         id = int(os.path.split(imagePath)[-1].split(".")[1])
+        
+        # Detect faces in the images
         faces = detector.detectMultiScale(img_numpy)
         
+        # Iterate over the detected faces
         for (x,y,w,h) in faces:
+            # Add the faces and its ID to the lists
             faceSamples.append(img_numpy[y:y+h,x:x+w])
             ids.append(id)
     return faceSamples,ids
 
+# Print a message to let the user know that the program is processing
 print ("\n [INFO] Training faces. It will take a few seconds. Wait ...")
+
+# Get the face samples and IDs
 faces,ids = getImagesAndLabels(path)
+
+# Train the model with the face sample and IDs
 recognizer.train(faces, np.array(ids))
 
 # Save the model into trainer/trainer.yml
 recognizer.write('trainer/trainer.yml') 
 
-# Print the numer of faces trained and end program
+# Print the numer of faces trained and end the program
 print("\n [INFO] {0} faces trained. Exiting Program".format(len(np.unique(ids))))
